c9a54254731160b8d5c2194d19b04fc34202d1c4
Mohammed <mogpt2x@gmail.com>
2024-12-12 12:24:45 -0800

    refactor main game loop to include board reset and improve turn management

--- tic-tac-toe/Sources/main.swift
+++ tic-tac-toe/Sources/main.swift
@@ -2,33 +2,44 @@
     build a game of tic-tac-toe using swift
 */
 
-
 print("\nWelcome to Tic Tac Toe!")
-
 
 var board: [[String]] = [
     ["1", "2", "3"],
     ["4", "5", "6"],
-    ["7", "8", "9"]
+    ["7", "8", "9"],
 ]
 
 var currentPlayerTurn: String = Bool.random() ? "X" : "O"
-printBoard()
 
-while !isBoardFull() {
-    takeTurn()
+// main game loop (reset, starting turn)
+while true {
+
+    resetBoard()
+    currentPlayerTurn = Bool.random() ? "X" : "O"
     printBoard()
-    if isWinner() {
-        print("Player \(currentPlayerTurn) has won !!")
+
+    // inner game loop ( turns, winner check)
+    while true {
+        takeTurn()
+        printBoard()
+        
+        if isWinner() {
+            print("Player \(currentPlayerTurn) has won !!")
+            break
+        }
+
+        if isBoardFull() {
+            print("nobody won this game...")
+            break
+        }
+
+        switchPlayerTurn()
+    }
+    
+    if !permissionToRestart() {
+        print("\n~~~Thanks for playing~~~\n")
         break
     }
 
-    switchPlayerTurn()
 }
-
-if isBoardFull() {
-    print("Nobody won ...")
-}
-
-
-

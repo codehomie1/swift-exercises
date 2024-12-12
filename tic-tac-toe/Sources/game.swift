6c3263d131f393972d809fa9181193e99962f5bf
Mohammed <mogpt2x@gmail.com>
2024-12-12 11:52:59 -0800

    refactor turn management and implement winner checking logic

--- tic-tac-toe/Sources/game.swift
+++ tic-tac-toe/Sources/game.swift
@@ -5,22 +5,45 @@
     and checking if games over. 
 */
 
-
-
-
 @MainActor
 func switchPlayerTurn() -> Void {
-    currentPlayerTurn  = currentPlayerTurn == 1 ? 0 : 1
+    currentPlayerTurn  = currentPlayerTurn == "X" ? "O" : "X"
 }
 
 @MainActor
 func takeTurn() -> Void {
     let (x,y) = getPlayerTurn()
-  
-    if currentPlayerTurn == 0 {
-        board[x][y] = "X"
-    } else {
-        board[x][y] = "O"
+    board[x][y] = currentPlayerTurn
+}
+
+@MainActor 
+func isWinner() -> Bool {
+
+    // check rows for winner
+    for row in board {
+        let checkRow = row[0] == currentPlayerTurn && row[1] == currentPlayerTurn 
+        && row[2] == currentPlayerTurn
+        if checkRow {
+            return true
+        }
     }
 
-}
+    // check cols
+    for col in 0...2 {
+        let checkCol: Bool = board[0][col] == currentPlayerTurn && 
+        board[1][col] == currentPlayerTurn && board[2][col] == currentPlayerTurn
+        if checkCol {
+            return true
+        }
+    }
+
+    // check rtl and ltr diag
+    if (board[0][0] == currentPlayerTurn && 
+    board[1][1] == currentPlayerTurn && board[2][2] == currentPlayerTurn) || 
+    (board[0][2] == currentPlayerTurn && board[1][1] == currentPlayerTurn && board[2][0] == currentPlayerTurn) 
+    {
+        return true
+    }
+
+    return false
+}   
